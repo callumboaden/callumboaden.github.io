6b8042690bfc2270689494e4b5527ddb030557a6
callumboaden <boaden.callum@gmail.com>
2026-06-05 12:22:53 +1000

    Add skill icons to portfolio

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ const header = document.querySelector("[data-header]");
 const year = document.querySelector("[data-year]");
 const revealItems = document.querySelectorAll(".reveal");
 const projectImages = document.querySelectorAll(".project-media img");
+const skillIconTargets = document.querySelectorAll(".skill-group h3[data-icon], .tag-list li[data-icon]");
+
+function createSkillIcon(iconName, className) {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+
+  svg.classList.add(className);
+  svg.setAttribute("aria-hidden", "true");
+  svg.setAttribute("focusable", "false");
+  use.setAttribute("href", `#icon-${iconName}`);
+  use.setAttributeNS("http://www.w3.org/1999/xlink", "href", `#icon-${iconName}`);
+  svg.append(use);
+
+  return svg;
+}
 
 if (year) {
   year.textContent = new Date().getFullYear();
 }
+
+skillIconTargets.forEach((item) => {
+  if (item.querySelector(".skill-heading-icon, .tag-icon")) return;
+
+  const iconName = item.dataset.icon;
+  const iconClass = item.matches("h3") ? "skill-heading-icon" : "tag-icon";
+  item.prepend(createSkillIcon(iconName, iconClass));
+});
 
 function setHeaderState() {
   if (!header) return;
